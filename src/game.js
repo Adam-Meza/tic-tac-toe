0cdc6e35a083b96666ed3be0c591a2567d297382
Adam Meza <Adammeza95@gmail.com>
2023-02-28 14:24:34 -0500

    initial commit

---
+++ src/game.js
@@ -0,0 +1,18 @@
+class Game {
+  constructor(player1_Inst, player2_Inst) {
+  }
+
+  trackTurn() {
+
+  }
+
+  checkWinConditions() {
+
+  }
+
+  resetBoard() {
+
+  }
+
+
+}
---
+++ src/player.js
@@ -0,0 +1,25 @@
+class player {
+  constructor(idInst,) {
+      this.id = idInst;
+      this.letter = letterIcon;
+      this.choices = {
+          A: [],
+          B: [],
+          C: [],
+          D: [],
+          E: [],
+          F: [],
+          G: [],
+          H: [],
+      }
+  }
+
+  addChoice() {
+
+  }
+
+  increaseWins() {
+
+  }
+
+}
