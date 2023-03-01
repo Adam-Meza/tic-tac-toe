9674f7708e96c4b40c728557337e218cb7211555
Adam Meza <Adammeza95@gmail.com>
2023-03-01 13:15:00 -0500

    Lay out updated data model

--- src/game.js
+++ src/game.js
@@ -1,26 +1,48 @@
 class Game {
-  constructor(player1_Inst, player2_Inst) {
-  }
+  constructor(firstPlayer, secondPlayer) {
+    this.firstPlayer = firstPlayer 
+    this.secondPlayer = secondPlayer
+    this.turn = 1
+    this.currentPlayer = {}
+    this.availableSquares = ["ADG", "BD", "CDH", "AE", "BEGH", "CE", "AFH", "BF", "CFG",]
+    this.choosenSquares = {
+      A: [],
+      B: [],
+      C: [],
+      D: [],
+      E: [],
+      F: [],
+      G: [],
+      H: [],
+      }
+    }
 
   trackTurn() {
-    // this is gonna use a counter and determine which players it is 
-    // but divisability of two
-    // if odds its players 1 if even its player two
+    this.turn += 1
+    if (this.turn % 2 === 0) {
+      this.currentPlayer = this.secondPlayer
+    } else {
+      this.currentPlayer = this.firstPlayer
+    }
+  };
 
-  }
+  checkWinConditions(currentPlayerInst) {
+    // instead of adding the square to the iwn condition add the oplayer's identifier
 
-  checkWinConditions() {
 // this is gonna trigger on click after the square has been assigned
 // this is gonna look at every array for each player and determine if there was a win
-// then it'll look if there are still squares left if not it declares a draw
-  }
+// then; it'll look if there are still squares left if not it declares a draw
+  };
 
   resetBoard() {
+    currentGame = new Game(this.secondPlayer, this.firstPlayer)
+    for (var i = 0; i < boardSquares.length; i++) {
+      boardSquares[i].disabled = false
+    }
 // this is gonna take away each player class from each swuare
 // its gonna activate all teh square buttons by iterating over the childNodes array
 // it'll change the header display to reflect whose turn it is
 // it'll create a new instace of game with the current players
-  }
-
+  };
 
 }
--- src/player.js
+++ src/player.js
@@ -1,24 +1,8 @@
-class player {
-  constructor(idInst,) {
-      this.id = idInst;
-      this.letter = letterIcon;
-      this.choices = {
-          A: [],
-          B: [],
-          C: [],
-          D: [],
-          E: [],
-          F: [],
-          G: [],
-          H: [],
-      }
-  }
-
-  addChoice() {
-// this is gonna look figure out whose turn it is
-// then is gonna at the square they selected 
-// then its gonna add it to each array hat it falls under ex: BD goes into B and D array
-
+class Player {
+  constructor(name, letter) {
+      this.name = name;
+      this.letter = letter;
+      this.wins = 0
   }
 
   increaseWins() {
