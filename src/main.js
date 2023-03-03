f53f47bfeb5f9eded8ca92fd0e4fce33c84c4c3f
Adam Meza <Adammeza95@gmail.com>
2023-03-03 11:09:03 -0500

    Finish basic functionality and layout of user name form

--- src/main.js
+++ src/main.js
@@ -1,17 +1,24 @@
 // GLOBAL DATA MODEL //
-var currentGame = new Game(new Player("Player 1", "X", "./assets/X_icon.jpg"), new Player("Player 2", "O", "./assets/O_icon.jpg"));
 var games = [];
+var firstPlayer = null;
+var secondPlayer = null;
 
 // QUERY SELECTORS //
 var turnHeader = document.querySelector('.js-turn-header');
 var gameBoard = document.querySelector('.js-game-board');
 var boardSquares = document.querySelectorAll('.js-board-square');
-var winBoxHeader = document.querySelectorAll('.js-win-box');
-var winCount = document.querySelectorAll('.js-win-count');
+var playerBoxes = document.querySelectorAll('.js-player-box')
+var winBoxHeaders = document.querySelectorAll('.js-win-box');
+var winCounts = document.querySelectorAll('.js-win-count');
 var newGameBtn = document.querySelector('.js-new-game-button');
+var playBtns = document.querySelectorAll('.js-play-btn')
+var playerNameTitles = document.querySelectorAll('.js-player-name')
+var nameInputs = document.querySelectorAll('.js-name-input')
+var nameForm = document.querySelector('.js-name-form')
+var secondNameForm = document.querySelector('.js-second-name-form')
 
 // EVENT LISTENERS // 
-window.addEventListener('load', setUpFirstGame);
+// window.addEventListener('load', setUpFirstGame);
 
 for (var i = 0; i < boardSquares.length; i++) {
   boardSquares[i].addEventListener('click', function() {
@@ -26,11 +33,33 @@ newGameBtn.addEventListener('click', function() {
   updateDOMForNewGame();
 })
 
+for (var i = 0; i <playBtns.length; i++) {
+  playBtns[i].addEventListener('click', function(event){
+    event.preventDefault()
+    storeNameInput()
+    pageNagivation()
+  }
+)}
+
+for (var i = 0; i < nameInputs.length; i++){
+  nameInputs[i].addEventListener('click', function(){
+  })
+}
+
 // DOM MANIPULATION - BUNDLE FUNCTIONS //
 
-function setUpFirstGame() {
-  currentGame.establishXandOPlayers();
-  updateTurnHeader();
+function pageNagivation(){
+  if (!secondPlayer) {
+    hide(nameForm)
+    show(secondNameForm)
+  } else if (firstPlayer && secondPlayer) {
+    setUpFirstGame()
+  }
+}
+
+function setUpFirstGame(){
+  updateDMforFirstGame()
+  updateDOMforFirstGame()
 }
 
 function updateDM() {
@@ -57,7 +86,49 @@ function updateDOMForNewGame() {
   updateWinCounter();
 }
 
-// DOM MANIPULATION - ATOMIC FUNCTIONS // 
+// DOM MANIPULATION - ATOMIC FUNCTIONS //
+
+function storeNameInput(){
+  if (nameInputs[0].value && !firstPlayer){
+    firstPlayer = new Player(`${nameInputs[0].value}`, "X", "./assets/X_icon.jpg")
+  } else if (nameInputs[1].value && !secondPlayer) {
+    secondPlayer = new Player(`${nameInputs[1].value}`, "O","./assets/O_icon.jpg" )
+  } else if (!nameInputs[0].value && !firstPlayer){
+    firstPlayer = new Player("Player 1", "X", "./assets/X_icon.jpg")
+  } else if (!nameInputs[1].value && !secondPlayer) {
+    secondPlayer = new Player("Player 2", "O", "./assets/O_icon.jpg")
+  }
+  clearInput()
+}
+
+function clearInput(){
+  nameInputs[0].value = ""
+  nameInputs[1].value = ""
+}
+
+function hide(element){
+  element.classList.add('hidden')
+}
+
+function show(element){
+  element.classList.remove('hidden')
+}
+
+function updateDMforFirstGame(){
+  currentGame = new Game (firstPlayer, secondPlayer)
+  currentGame.establishXandOPlayers();
+}
+
+function updateDOMforFirstGame(){
+  hide(secondNameForm)
+  show(gameBoard)
+  show(playerBoxes[0])
+  show(playerBoxes[1])
+  show(newGameBtn)
+  playerNameTitles[0].innerText = currentGame.firstPlayer.name
+  playerNameTitles[1].innerText = currentGame.secondPlayer.name
+  updateTurnHeader();
+}
 
 function updateTargetSquare() {
   event.target.disabled = true;
@@ -97,6 +168,6 @@ function resetDOM() {
 };
 
 function updateWinCounter() {
-    winCount[0].innerHTML = currentGame.xPlayer.wins;
-    winCount[1].innerHTML = currentGame.oPlayer.wins;
+    winCounts[0].innerHTML = currentGame.xPlayer.wins;
+    winCounts[1].innerHTML = currentGame.oPlayer.wins;
 };
